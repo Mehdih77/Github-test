420f26b04f2c9de2bf2813b5d3953a285a262598
Mehdih77 <mehdi.havaei77@gmail.com>
2021-10-27 09:57:40 +0330

    3

--- src/components/D/D.js
+++ src/components/D/D.js
@@ -3,7 +3,7 @@ import './D.css';
 export default function D() {
     return (
         <div className='d'>
-            D components / change
+            D components / change / 222
         </div>
     )
 }
